a33b5d2fc131d393cc386ea975a0c87e893e5900
bitich <bitich777@yahoo.co.jp>
2023-06-05 16:46:37 +0900

    コントローラーにリクエストを送信する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index  # 追記する
+    post = Post.create(content: params[:content]) # 追記する
+    redirect_to action: :index
   end
- 
-
 end
