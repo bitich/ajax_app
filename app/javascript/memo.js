70ce70b87c0f35d7d48d8f45f476223c284966a5
bitich <bitich777@yahoo.co.jp>
2023-06-10 18:03:25 +0900

    edit render-build

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -27,10 +27,10 @@ function post (){
     XHR.responseType = "json";
     XHR.send(formData);
     XHR.onload = () => {
-      if (XHR.status != 200 ){
-        alert(`Error ${XHR.status}: ${XHR.statusText}`);
-        return null;
-      };
+     if (XHR.status != 200 ){
+      alert(`Error ${XHR.status} ${XHR.statusText}` );
+      return null;
+     };
      const list = document.getElementById("list"); 
      const formText = document.getElementById("content");
      list.insertAdjacentHTML("afterend",buildHTML(XHR));
